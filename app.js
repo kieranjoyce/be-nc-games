eb4747e1aaa2225c54f216215d60158df9e3e570
kieranjoyce <kieran.joyce99@gmail.com>
2022-05-16 14:38:54 +0100

    create 405 and 500 error handling functions

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
-const { getCategories } = require('./controllers/games.controller');
+const { categoriesHandler } = require('./controllers/games.controller');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/api/categories', getCategories);
+app.all('/api/categories', categoriesHandler);
 
 app.all('/*', (req, res, next) => {
     res.status(404).send({msg: 'route not found'})
+})
+
+app.use((err, req, res, next) => {
+    if (err.msg && err.Allow) {
+        res.status(405).send({msg: err.msg, Allow: err.Allow});
+    }
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).send({msg: 'internal server error'})
 })
 
 module.exports = app;
