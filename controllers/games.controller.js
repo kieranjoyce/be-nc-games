451528d0e99313e5da0ab89fe4dd34d7c6b092ee
kieranjoyce <kieran.joyce99@gmail.com>
2022-05-16 14:39:51 +0100

    rename getCategories and add wrong method error handling

--- controllers/games.controller.js
+++ controllers/games.controller.js
@@ -1,9 +1,13 @@
 const { fetchCategories } = require("../models/games.model");
 
 
-exports.getCategories = (req, res, next) => {
-    fetchCategories()
-    .then(categories => {
-        res.status(200).send({categories});
-    })
+exports.categoriesHandler = (req, res, next) => {
+    if (req.method === 'GET') {
+        fetchCategories()
+        .then(categories => {
+            res.status(200).send({categories});
+        })
+    } else {
+        next({msg: `${req.method} method not supported for this route`, Allow: ['GET']})
+    }
 };
