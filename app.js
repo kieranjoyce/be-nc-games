4725873e2e96eaf0f1de67718bdb55ce16ef55a4
kieranjoyce <kieran.joyce99@gmail.com>
2022-05-16 13:15:42 +0100

    add custom 404 error handling

--- app.js
+++ app.js
@@ -7,5 +7,8 @@ app.use(express.json());
 
 app.get('/api/categories', getCategories);
 
+app.all('/*', (req, res, next) => {
+    res.status(404).send({msg: 'route not found'})
+})
 
 module.exports = app;
