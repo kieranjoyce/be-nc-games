b8deb354f6f91e28f5a3bddc093894f796cab36d
kieranjoyce <kieran.joyce99@gmail.com>
2022-05-16 15:46:04 +0100

    refactor first endpoint to use .route()

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express');
-const { categoriesHandler } = require('./controllers/games.controller');
+const { getCategories, methodNotAllowedHandler } = require('./controllers/games.controller');
 
 const app = express();
 
 app.use(express.json());
 
-app.all('/api/categories', categoriesHandler);
+app.route('/api/categories')
+    .get(getCategories)
+    .all(methodNotAllowedHandler(['GET']));
 
 app.all('/*', (req, res, next) => {
     res.status(404).send({msg: 'route not found'})
 })
 
 app.use((err, req, res, next) => {
-    if (err.msg && err.Allow) {
+    if (err.status === 405) {
         res.status(405).send({msg: err.msg, Allow: err.Allow});
     }
 })
