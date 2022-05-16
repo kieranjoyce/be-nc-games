22bd697b0b78b99bdeb1e3cd3bfd4b9e27e40647
kieranjoyce <kieran.joyce99@gmail.com>
2022-05-16 17:10:46 +0100

    create getReview function

--- controllers/games.controller.js
+++ controllers/games.controller.js
@@ -1,4 +1,4 @@
-const { fetchCategories } = require("../models/games.model");
+const { fetchCategories, fetchReview } = require("../models/games.model");
 
 
 exports.getCategories = (req, res, next) => {
@@ -7,6 +7,17 @@ exports.getCategories = (req, res, next) => {
             res.status(200).send({categories});
         });
 };
+
+exports.getReview = (req, res, next) => {
+    const {review_id} = req.params;
+    fetchReview(review_id)
+        .then(review => {
+            res.status(200).send({ review });
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
 
 exports.methodNotAllowedHandler = (allowedMethods) => ((req, res, next) => {
     next({status: 405, msg: `${req.method} method not supported for this route`, Allow: allowedMethods})
