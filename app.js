edd8087a5679f0ddea42349f76a30656d2815d5b
kieranjoyce <kieran.joyce99@gmail.com>
2022-05-16 12:44:06 +0100

    set up GET /api/categories endpoint

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const { getCategories } = require('./controllers/games.controller');
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/api/categories', getCategories);
 
 
 module.exports = app;
