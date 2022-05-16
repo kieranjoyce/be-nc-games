2803f28dfd9a9cbf77444f587dbd6cb717559ebf
kieranjoyce <kieran.joyce99@gmail.com>
2022-05-16 17:11:15 +0100

    create fetchReview function

--- models/games.model.js
+++ models/games.model.js
@@ -8,3 +8,13 @@ exports.fetchCategories = () => {
             return categories;
         })
 };
+
+exports.fetchReview = (review_id) => {
+    return db.query(`
+        SELECT * FROM reviews
+        WHERE review_id=$1
+    `, [review_id])
+    .then(({rows : [review]}) => {
+        return review;
+    })
+}
