e93b9cd9082c5bf41cb047e52164ce2d380deb06
kieranjoyce <kieran.joyce99@gmail.com>
2022-05-17 15:12:40 +0100

    update patchReview with response and error handling

--- controllers/reviews.controller.js
+++ controllers/reviews.controller.js
@@ -14,8 +14,18 @@ exports.getReview = (req, res, next) => {
 exports.patchReview = (req, res, next) => {
     const {review_id} = req.params;
     const {inc_votes} = req.body;
+
+    if (!inc_votes) {
+        next({status: 400, msg: 'request must have inc_votes property'});
+    };
+    if (Object.keys(req.body).length > 1) {
+        next({status: 400, msg: 'request must only have inc_votes property'});
+    }
+
+
     updateReview(review_id, inc_votes)
     .then((review) => {
-        console.log(review);
+        res.status(200).send({review});
     })
+    .catch(next);
 }
