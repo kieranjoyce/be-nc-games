865c219606f4358e94e0f4914e5a518262ce2282
kieranjoyce <kieran.joyce99@gmail.com>
2022-05-17 11:31:15 +0100

    invoke next with err in catch block

--- controllers/reviews.controller.js
+++ controllers/reviews.controller.js
@@ -7,6 +7,6 @@ exports.getReview = (req, res, next) => {
             res.status(200).send({ review });
         })
         .catch(err => {
-            console.log(err);
+            next(err);
         })
 }
