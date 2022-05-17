c34c70446a7286c106e3b2d850b39819aa40ee3c
kieranjoyce <kieran.joyce99@gmail.com>
2022-05-17 11:30:29 +0100

    create and export psql and custom error handling functions

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -9,3 +9,19 @@ exports.routeNotFoundHandler = (req, res, next) => {
 exports.internalServerErrorHandler = (err, req, res, next) => {
     res.status(500).send({msg: 'internal server error'});
 }
+
+exports.psqlErrorHandler = (err, req, res, next) => {
+    if (err.code === '22P02') {
+        res.status(400).send({msg : 'invalid review_id, expecting number data type'})
+    } else {
+        next(err);
+    }
+}
+
+exports.customErrorHandler = (err, req, res, next) => {
+    if (err.status && err.msg) {
+        res.status(err.status).send({msg: err.msg});
+    } else {
+        next(err);
+    }
+}
