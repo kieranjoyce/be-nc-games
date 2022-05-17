34fa02b39f6c009b093a66ff15cef08a4a0f05b7
kieranjoyce <kieran.joyce99@gmail.com>
2022-05-17 15:13:34 +0100

    update updateReview with query result handling

--- models/reviews.model.js
+++ models/reviews.model.js
@@ -20,5 +20,13 @@ exports.updateReview = (review_id, inc_votes) => {
         UPDATE reviews
         SET votes = votes + $2
         WHERE review_id = $1
+        RETURNING *;
     `, [review_id, inc_votes])
+    .then(({rows : [review]}) => {
+        if (review) {
+            return review;
+        } else {
+            return Promise.reject({status: 404, msg: `no review found for id: ${review_id}`})
+        }
+    })
 }
