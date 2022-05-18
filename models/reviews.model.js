cb174ecbf7960db827178efe545710584d57ee46
kieranjoyce <kieran.joyce99@gmail.com>
2022-05-18 14:16:22 +0100

    update SQL query to provide comment_count data

--- models/reviews.model.js
+++ models/reviews.model.js
@@ -3,8 +3,11 @@ const db = require("../db/connection");
 
 exports.fetchReview = (review_id) => {
     return db.query(`
-        SELECT * FROM reviews
-        WHERE review_id=$1
+        SELECT reviews.*, COUNT(comments.comment_id)::INT AS comment_count
+        FROM reviews LEFT JOIN comments 
+        ON reviews.review_id = comments.review_id
+        WHERE reviews.review_id=$1
+        GROUP BY reviews.review_id;
     `, [review_id])
     .then(({rows : [review]}) => {
         if (review) {
