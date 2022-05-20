1af59852b8b17e78641245cc80b0527181e81388
kieranjoyce <kieran.joyce99@gmail.com>
2022-05-20 11:53:18 +0100

    create comments.controller.js and deleteComment function

---
+++ controllers/comments.controller.js
@@ -0,0 +1,11 @@
+const { removeComment } = require("../models/comments.model")
+
+exports.deleteComment = (req, res, next) => {
+    const { comment_id } = req.params;
+
+    removeComment(comment_id)
+        .then(() => {
+            res.status(204).end();
+        })
+        .catch(next);
+}
