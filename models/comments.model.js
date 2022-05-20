1e8f7651b075abfb802209f90c33f510eb54d971
kieranjoyce <kieran.joyce99@gmail.com>
2022-05-20 12:04:50 +0100

    add error handling to removeComment function

--- models/comments.model.js
+++ models/comments.model.js
@@ -3,6 +3,12 @@ const db = require("../db/connection")
 exports.removeComment = (comment_id) => {
     return db.query(`
         DELETE FROM comments 
-        WHERE comment_id=$1;
+        WHERE comment_id=$1
+        RETURNING *;
     `, [comment_id])
+    .then(({ rows : deletedComment}) => {
+        if (deletedComment.length === 0) {
+            return Promise.reject({status:404, msg: `no comment found for id: ${comment_id}`})
+        }
+    })
 }
