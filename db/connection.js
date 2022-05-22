ac43808290cf91039c156fa585f28727506c5eb0
kieranjoyce <kieran.joyce99@gmail.com>
2022-05-22 17:25:19 +0100

    fixed config not being used

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
-
+console.log(ENV);
 require('dotenv').config({
     path: `${__dirname}/../.env.${ENV}`,
 });
@@ -18,4 +18,4 @@ const config = ENV === 'production' ?
     }
     : {};
 
-module.exports = new Pool();
+module.exports = new Pool(config);
